83febf3e270490909b1038c04e693e66423e63a8
osmarmora05 <osmaradrianmoracer@gmail.com>
2024-03-14 18:15:05 -0600

    chore(notFond): set fixed gap between title & info

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,15 +1,20 @@
 import { RobotIcon } from "../components/ui/Icons";
-import '../css/notfound.css'
+import "../css/notfound.css";
 
 function NotFound() {
   return (
-    <div className="not-found">
-      <div className="not-found__404-box">
-        <RobotIcon />
-        <h1 className="not-found__404">404</h1>
+    <section className="not-found">
+      <div className="not-found__box">
+        <div className="not-found__404-icon-and-title-box">
+          <RobotIcon />
+          <h1 className="not-found__404">404</h1>
+        </div>
+        <h2 className="not-found__text">
+          La página que buscas no se encuentra aquí. <br /> ¿Por qué no intentas
+          regresar a la pagina de inicio?
+        </h2>
       </div>
-      <h2 className="not-found__text">La página que buscas no se encuentra aquí. <br/> ¿Por qué no intentas regresar a la pagina de inicio?</h2>
-    </div>
+    </section>
   );
 }
 
